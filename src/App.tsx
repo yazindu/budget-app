92f34d05831fbb55867fddc6e1816adf73d6ba1e
yasindu <yazindu@gmail.com>
2023-08-25 07:13:35 +0530

    installed uuid, started coding html css basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import {Button, Container, Stack} from "react-bootstrap";
+
 function App() {
 
     return (
-        <>
-            <h1>Hello</h1>
-        </>
+        <Container className={'my-4'}>
+            <Stack direction={'horizontal'} gap={2} className={'mb-4'}>
+                <h1 className={'me-auto'}>Budgets</h1>
+                <Button variant={'primary'}>Add Budget</Button>
+                <Button variant={'outline-primary'}>Add Expense</Button>
+            </Stack>
+        </Container>
     )
 }
 
